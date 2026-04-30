dc1f0639c9e58bf1a6c3c95ccdd06c944ea13209
MagicCrazyMan <350088648@qq.com>
2026-04-30 16:29:14 +0800

    Fit to the up-to-date running environment

--- convert.py
+++ convert.py
@@ -16,17 +16,18 @@ import shutil
 
 # This Python script is based on the shell converter script provided in the MipNerF 360 repository.
 parser = ArgumentParser("Colmap converter")
-parser.add_argument("--no_gpu", action='store_true')
+# parser.add_argument("--no_gpu", action='store_true') // always use GPU
 parser.add_argument("--skip_matching", action='store_true')
 parser.add_argument("--source_path", "-s", required=True, type=str)
 parser.add_argument("--camera", default="OPENCV", type=str)
 parser.add_argument("--colmap_executable", default="", type=str)
-parser.add_argument("--resize", action="store_true")
-parser.add_argument("--magick_executable", default="", type=str)
+# parser.add_argument("--resize", action="store_true")
+# parser.add_argument("--magick_executable", default="", type=str)
 args = parser.parse_args()
 colmap_command = '"{}"'.format(args.colmap_executable) if len(args.colmap_executable) > 0 else "colmap"
-magick_command = '"{}"'.format(args.magick_executable) if len(args.magick_executable) > 0 else "magick"
-use_gpu = 1 if not args.no_gpu else 0
+# magick_command = '"{}"'.format(args.magick_executable) if len(args.magick_executable) > 0 else "magick"
+use_gpu = 1
+# use_gpu = 1 if not args.no_gpu else 0
 
 if not args.skip_matching:
     os.makedirs(args.source_path + "/distorted/sparse", exist_ok=True)
@@ -37,7 +38,7 @@ if not args.skip_matching:
         --image_path " + args.source_path + "/input \
         --ImageReader.single_camera 1 \
         --ImageReader.camera_model " + args.camera + " \
-        --SiftExtraction.use_gpu " + str(use_gpu)
+        --FeatureExtraction.use_gpu " + str(use_gpu)
     exit_code = os.system(feat_extracton_cmd)
     if exit_code != 0:
         logging.error(f"Feature extraction failed with code {exit_code}. Exiting.")
@@ -46,7 +47,7 @@ if not args.skip_matching:
     ## Feature matching
     feat_matching_cmd = colmap_command + " exhaustive_matcher \
         --database_path " + args.source_path + "/distorted/database.db \
-        --SiftMatching.use_gpu " + str(use_gpu)
+        --FeatureMatching.use_gpu " + str(use_gpu)
     exit_code = os.system(feat_matching_cmd)
     if exit_code != 0:
         logging.error(f"Feature matching failed with code {exit_code}. Exiting.")
@@ -55,7 +56,9 @@ if not args.skip_matching:
     ### Bundle adjustment
     # The default Mapper tolerance is unnecessarily large,
     # decreasing it speeds up bundle adjustment steps.
-    mapper_cmd = (colmap_command + " mapper \
+    #
+    # GLOMAP is merged into COLMAP now, use it
+    mapper_cmd = (colmap_command + " global_mapper \
         --database_path " + args.source_path + "/distorted/database.db \
         --image_path "  + args.source_path + "/input \
         --output_path "  + args.source_path + "/distorted/sparse \
@@ -87,38 +90,39 @@ for file in files:
     destination_file = os.path.join(args.source_path, "sparse", "0", file)
     shutil.move(source_file, destination_file)
 
-if(args.resize):
-    print("Copying and resizing...")
+# A standalone program performs image resizing, not necessary here.
+# if(args.resize):
+#     print("Copying and resizing...")
 
-    # Resize images.
-    os.makedirs(args.source_path + "/images_2", exist_ok=True)
-    os.makedirs(args.source_path + "/images_4", exist_ok=True)
-    os.makedirs(args.source_path + "/images_8", exist_ok=True)
-    # Get the list of files in the source directory
-    files = os.listdir(args.source_path + "/images")
-    # Copy each file from the source directory to the destination directory
-    for file in files:
-        source_file = os.path.join(args.source_path, "images", file)
+#     # Resize images.
+#     os.makedirs(args.source_path + "/images_2", exist_ok=True)
+#     os.makedirs(args.source_path + "/images_4", exist_ok=True)
+#     os.makedirs(args.source_path + "/images_8", exist_ok=True)
+#     # Get the list of files in the source directory
+#     files = os.listdir(args.source_path + "/images")
+#     # Copy each file from the source directory to the destination directory
+#     for file in files:
+#         source_file = os.path.join(args.source_path, "images", file)
 
-        destination_file = os.path.join(args.source_path, "images_2", file)
-        shutil.copy2(source_file, destination_file)
-        exit_code = os.system(magick_command + " mogrify -resize 50% " + destination_file)
-        if exit_code != 0:
-            logging.error(f"50% resize failed with code {exit_code}. Exiting.")
-            exit(exit_code)
+#         destination_file = os.path.join(args.source_path, "images_2", file)
+#         shutil.copy2(source_file, destination_file)
+#         exit_code = os.system(magick_command + " mogrify -resize 50% " + destination_file)
+#         if exit_code != 0:
+#             logging.error(f"50% resize failed with code {exit_code}. Exiting.")
+#             exit(exit_code)
 
-        destination_file = os.path.join(args.source_path, "images_4", file)
-        shutil.copy2(source_file, destination_file)
-        exit_code = os.system(magick_command + " mogrify -resize 25% " + destination_file)
-        if exit_code != 0:
-            logging.error(f"25% resize failed with code {exit_code}. Exiting.")
-            exit(exit_code)
+#         destination_file = os.path.join(args.source_path, "images_4", file)
+#         shutil.copy2(source_file, destination_file)
+#         exit_code = os.system(magick_command + " mogrify -resize 25% " + destination_file)
+#         if exit_code != 0:
+#             logging.error(f"25% resize failed with code {exit_code}. Exiting.")
+#             exit(exit_code)
 
-        destination_file = os.path.join(args.source_path, "images_8", file)
-        shutil.copy2(source_file, destination_file)
-        exit_code = os.system(magick_command + " mogrify -resize 12.5% " + destination_file)
-        if exit_code != 0:
-            logging.error(f"12.5% resize failed with code {exit_code}. Exiting.")
-            exit(exit_code)
+#         destination_file = os.path.join(args.source_path, "images_8", file)
+#         shutil.copy2(source_file, destination_file)
+#         exit_code = os.system(magick_command + " mogrify -resize 12.5% " + destination_file)
+#         if exit_code != 0:
+#             logging.error(f"12.5% resize failed with code {exit_code}. Exiting.")
+#             exit(exit_code)
 
 print("Done.")
